4b08f6c675be8a6ae3dc89cc742bf0e9096ea14f
Rose22 <rosemelody22@gmail.com>
2026-03-02 01:55:00 +0100

    default headers

--- tools/http.py
+++ tools/http.py
@@ -4,7 +4,7 @@ import requests
 class HttpTool(core.tool.Tool):
     """lets the AI send/receive HTTP requests"""
 
-    async def get(self, url: str, headers: dict, params: dict = None):
+    async def get(self, url: str, headers: dict = None, params: dict = None):
         """
         performs a HTTP GET request on url
 
@@ -13,13 +13,16 @@ class HttpTool(core.tool.Tool):
             params: any parameters to add to the request (shows up as the ?q=blah&si=blah2 part of the url)
             headers: HTTP headers. You want to always set these!
         """
+        if not headers:
+            headers = {'User-Agent': 'Mozilla/5.0'}
+
         result = requests.get(url, params=params, headers=headers)
         return self.result({
             "status": f"{result.status_code} {result.reason}",
             "content": result.text
         })
 
-    async def post(self, url: str, headers: dict, data: dict = None):
+    async def post(self, url: str, headers: dict = None, data: dict = None):
         """
         performs a HTTP POST on url
 
@@ -28,5 +31,8 @@ class HttpTool(core.tool.Tool):
             data: the data to post to the url
             headers: HTTP headers. You want to always set these!
         """
+        if not headers:
+            headers = {'User-Agent': 'Mozilla/5.0'}
+
         result = requests.post(url, data, headers=headers)
         return self.result(result.text)
